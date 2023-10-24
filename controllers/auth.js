32e2d3aa7252e5617811f80c010806975632175f
Javier Moneo <jvr.moneo@gmail.com>
2023-10-24 19:56:56 +0200

    Recuperar información de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,33 @@
 const { response } = require("express");
 
 const crearUsuario = (req, res = response) => {
-  res.json({
+  // console.log(req);
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El nombre debe de ser de 5 letras",
+    });
+  }
+
+  return res.json({
     ok: true,
     msg: "registro",
+    name,
+    email,
+    password,
   });
 };
 
 const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
