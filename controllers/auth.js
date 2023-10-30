0dff1afe9299b026b84b237a79b1af8fe8834b47
Javier Moneo <jvr.moneo@gmail.com>
2023-10-30 12:31:18 +0100

    Custom Middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,8 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   // console.log(req);
   const { name, email, password } = req.body;
-
-  // manejo de errores
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   return res.status(201).json({
     ok: true,
@@ -25,15 +15,6 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
   const { email, password } = req.body;
-
-  // manejo de errores
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.json({
     ok: true,
